aa10990191a8f33ff0a2d351d9100b801e4fab38
Tuyen Ha <tuyen_ha@outlook.com>
2022-11-03 11:14:31 -0600

    commit tree e830f023f5dd1c14899ad1a0d1ce40d6f40dd6e9

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
+# Using while or until loop, output numbers starting at 10 and counting down by 1. After reaching 1, print out "HAPPY NEW YEARS!"
 def happy_new_year
-  # your code here
+  i = 10
+  while i > 0
+    puts "#{i}"
+    puts "Happy New Year!" if i == 1
+    i -= 1
+  end
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -14,11 +20,21 @@ def fizzbuzz(num)
     num
   end
 end
-
+#(Inside of fizzbuzz_printer we want to call the fizzbuzz method)
+# Print numbers from 1 to 100. For multiples of three, print "Fizz" instead of the number and for multiples of five print "Buzz." For multiples of both three and five, print "FizzBuzz."
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
+# Takes in onr argument, a string, and reverse it without using built-in methods. Loop through the characters in the input string and reverse it.
 def reverse_string(str)
-  # your code here
+  rev = ""
+  for i in 1..str.length
+    puts str[i]
+    rev += str[str.length - i]
+  end
+  puts rev
 end
+reverse_string("hello")
